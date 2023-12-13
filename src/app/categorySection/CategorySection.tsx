75f798e90a374bffc076c33acb82b9e4f0295b2a
Maria Kazakova <mariakazakova92@gmail.com>
2023-12-13 14:31:26 +0300

    added css styles to the category section

--- src/app/categorySection/CategorySection.tsx
+++ src/app/categorySection/CategorySection.tsx
@@ -1,12 +1,14 @@
 import { CATEGORY } from "@/app/constants/constants";
 
-const CategorySection = () =>{
-
-  return (
-    CATEGORY.map((category)=>{
-      return <div key={category}><p>{category}</p></div>
-    })
-
+const CategorySection = () => {
+  const categoryList = CATEGORY.map((category)=>{
+    return <li key={category} className={'border-2 border-blue-300 rounded-full p-1.5 px-2 hover:border-blue-500'}>{category}</li>
+  })
+  return (<nav className={'max-w-4xl my-1 mx-auto'}>
+        <ul className={'flex justify-around '}>
+           {categoryList}
+        </ul>
+    </nav>
   )
 }
 
