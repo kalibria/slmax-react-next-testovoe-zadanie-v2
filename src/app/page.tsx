4d998a32f58ce3597aa5cf48399574483a0994a5
Maria Kazakova <mariakazakova92@gmail.com>
2023-12-13 19:51:56 +0300

    added Link on the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      <h1>Hello world!</h1>
+      <div className={'border-2 border-blue-300 rounded-full p-1.5 px-2  hover:bg-blue-50 w-28 text-center my-3 mx-auto'}>
+        <Link href={'/photos'} >PHOTOS</Link>
+      </div>
 
     </main>
   )
