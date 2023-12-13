0f82c65f2eba197348442d26ceb600ac85572a1c
Maria Kazakova <mariakazakova92@gmail.com>
2023-12-13 20:44:10 +0300

    added styles to the auth form

--- src/app/auth/AuthForm.tsx
+++ src/app/auth/AuthForm.tsx
@@ -1,19 +1,24 @@
+import styles from '@/app/auth/auth.module.css'
+
 const AuthForm = () =>{
   return (
-    <form>
-      <fieldset>
-        <input type='text' id='name' name='name' required minLength="4"
-               maxLength="8" placeholder="Enter name" />
-        <label htmlFor='name'>Min 4 max 8 characters</label>
-      </fieldset>
+    <div className={styles.authWrapper}>
+      <form className={'w-44 m-auto '}>
+        <fieldset className={'mb-5'}>
+          <input type='text' id='name' name='name' required minLength="4"
+                 maxLength="8" placeholder="Enter name" />
+          <label htmlFor='name' className={'text-xs'}>Min 4 max 8 characters</label>
+        </fieldset>
 
-      <fieldset>
-        <input type='password' id='password' name='password' required
-               minLength="4" maxLength="8" placeholder="Enter password" />
-        <label htmlFor='password'>Min 4 max 8 characters</label>
-      </fieldset>
+        <fieldset>
+          <input type='password' id='password' name='password' required
+                 minLength="4" maxLength="8" placeholder="Enter password" />
+          <label htmlFor='password' className={'text-xs'}>Min 4 max 8 characters</label>
+        </fieldset>
 
-    </form>
+      </form>
+    </div>
+
   )
 }
 export default AuthForm;
