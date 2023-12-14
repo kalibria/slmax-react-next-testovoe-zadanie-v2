af2b74b4740b1cf66fcb75769ebbe687cbf232e0
Maria Kazakova <mariakazakova92@gmail.com>
2023-12-14 13:29:56 +0300

    added navigate from photos page to the myCollection page

--- src/app/myCollection/LikedCollection.tsx
+++ src/app/myCollection/LikedCollection.tsx
@@ -1,7 +1,14 @@
+import { useRouter } from "next/navigation";
+
 const LikedCollection =() =>{
+  const router = useRouter();
+
+  const handleClick = () =>{
+    router.push('/myCollection')
+  }
   return (
     <div className={'flex justify-center'}>
-      <button className={'border-2 border-blue-300 rounded-full p-1.5 px-2  hover:bg-blue-50 mb-6 '}>My collection</button>
+      <button className={'border-2 border-blue-300 rounded-full p-1.5 px-2  hover:bg-blue-50 mb-6 '} onClick={handleClick}>My collection</button>
     </div>
 
   )
