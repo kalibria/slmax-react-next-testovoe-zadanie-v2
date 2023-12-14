4bf9ed03a63aa6daf8a88cd527f694b786076c8c
Maria Kazakova <mariakazakova92@gmail.com>
2023-12-14 10:01:54 +0300

    changed enter button

--- src/app/auth/AuthForm.tsx
+++ src/app/auth/AuthForm.tsx
@@ -3,7 +3,8 @@ import styles from '@/app/auth/auth.module.css'
 const AuthForm = () =>{
   return (
     <div className={styles.authWrapper}>
-      <form className={'w-44 m-auto '}>
+      <h2 className={'text-xl font-bold italic text-center mb-6'}>Log in</h2>
+      <form className={'w-44 my-0 mx-auto '}>
         <fieldset className={'mb-5'}>
           <input type='text' id='name' name='name' required minLength="4"
                  maxLength="8" placeholder="Enter name" />
@@ -15,6 +16,9 @@ const AuthForm = () =>{
                  minLength="4" maxLength="8" placeholder="Enter password" />
           <label htmlFor='password' className={'text-xs'}>Min 4 max 8 characters</label>
         </fieldset>
+
+
+          <button type='submit' className={'border-2 border-blue-300 w-full mt-6 p-1.5 px-2  hover:bg-blue-50 '}>Enter</button>
 
       </form>
     </div>
